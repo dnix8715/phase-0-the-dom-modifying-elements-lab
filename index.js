11119fd74d4557c487d4ecbd3dc9ad4df51aff7e
Dominic nix <dnix8715@gmail.com>
2024-01-27 23:13:11 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+// index.js
+
+// Create a new header element
+const newHeader = document.createElement("h1");
+
+// Set the id of the header to 'victory'
+newHeader.id = "victory";
+
+// Set the text content of the header
+newHeader.textContent = "Dominic is the champion";
+
+// Append the header to the main element in the DOM
+document.getElementById("main").appendChild(newHeader);
+document.body.appendChild(newHeader);
+document.getElementById("main").remove();
